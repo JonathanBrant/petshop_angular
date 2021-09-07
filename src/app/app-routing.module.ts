81d0aabdc8cae840147b9d741696cd81c611cd7d
Jonathan Brant <jbrant1995@gmail.com>
2021-09-06 22:32:45 -0300

    adicionados componentes e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { ContactComponent } from './components/contact/contact.component';
+import { ServicesComponent } from './components/services/services.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: "",
+      component: AboutComponent,
+    },
+    {
+      path: "service",
+      component: ServicesComponent
+    },
+    {
+      path: "contact",
+      component: ContactComponent,
+    },
+    {
+      path: "signin",
+      component: SigninComponent,
+    },
+    {
+      path: "signup",
+      component: SignupComponent,
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FooterInfoComponent } from './components/footer-info/footer-info.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FooterInfoComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
